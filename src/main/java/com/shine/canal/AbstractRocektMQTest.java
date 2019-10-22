0276504a28af12c599d15ff9afce5f384deeaea8
肖显明 <531088309@qq.com>
2019-10-22 17:06:54 +0800

    add canal-rocketmq demo

---
+++ src/main/java/com/shine/canal/AbstractRocektMQTest.java
@@ -0,0 +1,13 @@
+package com.shine.canal;
+
+public abstract class AbstractRocektMQTest extends BaseCanalClientTest {
+
+    public static String topic       = "example";
+    public static String groupId     = "test";
+    public static String nameServers = "192.168.186.129:9876";
+    public static String accessKey   = "";
+    public static String secretKey   = "";
+    public static boolean enableMessageTrace = true;
+    public static String accessChannel = "local";
+    public static String namespace = "";
+}
